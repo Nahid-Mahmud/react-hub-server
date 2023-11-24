8ea65a49e37d0cb424da544e2901e48921c31043
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2023-11-24 20:27:00 +0600

    user saved in database

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
-const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 // mongodb codes
 
@@ -23,6 +27,25 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // my codes here
+    const usersCollection = client.db("reactHubDb").collection("users");
+
+    // post users
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      //   console.log(user);
+      const query = { email: user.email };
+
+      const existingUser = await usersCollection.findOne(query);
+      //   console.log(existingUser);
+      if (existingUser) {
+        return res.send({ message: "User already exists" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -30,7 +53,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
