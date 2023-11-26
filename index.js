f2a35abe2b9e3263905c023715f6699c8f264056
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2023-11-26 20:46:00 +0600

    user role update upon pyment api added and also transaction id saved

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
+// console.log(process.env.STRIPE_SECRET_KEY)
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -81,7 +83,7 @@ async function run() {
       res.send({ admin });
     });
 
-    // get user data 
+    // get user data
     app.get("/user/:email", veryfyToken, async (req, res) => {
       const email = req.params.email;
       if (email !== req.decoded.email) {
@@ -90,6 +92,29 @@ async function run() {
       const query = { email: email };
       const user = await usersCollection.findOne(query);
       res.send(user);
+    });
+
+    // update user role
+    app.put("/user/role/:email", veryfyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(401).send({ message: "Unauthorized Request" });
+      }
+      const query = { email: email };
+      const updateRole = req.body;
+      const updateUserRole = {
+        $set: {
+          role: updateRole.role,
+          paymentId: updateRole.paymentId,
+        },
+      };
+      options = { upsert: true };
+      const result = await usersCollection.updateOne(
+        query,
+        updateUserRole,
+        options
+      );
+      res.send(result);
     });
 
     // jwt related apis
@@ -246,6 +271,20 @@ async function run() {
       const query = { email: req.params.email };
       const comments = await commentsCollection.find(query).toArray();
       res.send({ totalUserComments: comments.length });
+    });
+
+    // payment related api
+    // create payment intent
+    app.get("/create-payment-intent", veryfyToken, async (req, res) => {
+      const price = 500;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: price,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
 
     // Send a ping to confirm a successful connection
